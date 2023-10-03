aa981dc4423b910c337cace23da733e04dd9393e
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-10-03 18:49:48 +0500

    add fragments

---
+++ app/src/main/java/com/example/game_flystars/ui/home/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.example.game_flystars.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.game_flystars.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
