e56e8843c55fdac2ec1ea78d6617f22ce36f45e0
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-10-04 11:18:47 +0500

    add rules fragment

--- app/src/main/java/com/example/game_flystars/ui/rules/RulesFragment.kt
+++ app/src/main/java/com/example/game_flystars/ui/rules/RulesFragment.kt
@@ -5,13 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.game_flystars.R
+import com.example.game_flystars.databinding.FragmentRulesBinding
 
 class RulesFragment : Fragment() {
+
+    private lateinit var binding: FragmentRulesBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_rules, container, false)
+    ): View {
+        binding = FragmentRulesBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply { btnBack.setOnClickListener { findNavController().popBackStack() } }
     }
 }
