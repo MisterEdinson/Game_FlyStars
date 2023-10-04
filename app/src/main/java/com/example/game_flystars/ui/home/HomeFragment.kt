4272e7badcd5b793698217b1f6a442ea71f12f63
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-10-04 10:58:29 +0500

    add home fragment

--- app/src/main/java/com/example/game_flystars/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/game_flystars/ui/home/HomeFragment.kt
@@ -5,14 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.game_flystars.R
+import com.example.game_flystars.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            btnStartGame.setOnClickListener { findNavController().navigate(R.id.action_homeFragment_to_gameFragment) }
+            btnRulesGame.setOnClickListener { findNavController().navigate(R.id.action_homeFragment_to_rulesFragment) }
+            btnExitGame.setOnClickListener { activity?.finish() }
+        }
     }
 }
